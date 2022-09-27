16c75fa491b9ec2736d5966faa1dd629c9ae7ae9
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-09-26 23:26:52 -0300

    Add files via upload

---
+++ saude_mental_paises_capitalistas_comunistas.R
@@ -0,0 +1,14 @@
+
+# Saúde Mental  ----------------------------------------------------------------------------------------------------------------------------
+# Autoria do script: Jeanne Franco ---------------------------------------------------------------------------------------------------------
+# Data 26/09/22 ----------------------------------------------------------------------------------------------------------------------------
+# Referência: https://ourworldindata.org/mental-health -------------------------------------------------------------------------------------
+
+# Sobre os dados ---------------------------------------------------------------------------------------------------------------------------
+
+### Nesta entrada apresentamos as últimas estimativas da prevalência de 
+### doenças mentais e da carga das doenças associadas. A maioria das estimativas
+### apresentadas foram produzidas por: Institute for Health Metrics and Evaluation
+### e relatados pelo porta bandeira Global Burden of Disease study deles.
+
+
