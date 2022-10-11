bfe80e6e62f9a2ac71bc79755ed740b162dfecd9
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-10-11 20:46:44 -0300

    Add files via upload

--- saude_mental_paises_capitalistas_comunistas.R
+++ saude_mental_paises_capitalistas_comunistas.R
@@ -98,7 +98,8 @@ c4a("dark2", 2)
 ggplot(sm3, aes(x = Year, y = por_saude_ment, 
                   group = Entity, col = Entity)) +
   geom_line(size = 2.2) +
-  scale_color_manual(values = c("#1B9E77", "#D95F02")) +
+  scale_color_manual(values = c("#1B9E77", "#D95F02"),
+                     labels = c("China", "Estados Unidos")) +
   labs(x = "Tempo (anos)", y = "Transtornos mentais (%)", 
        color = "Países") +
   theme_hc() +
